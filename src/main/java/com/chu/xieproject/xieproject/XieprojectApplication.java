3d8174e051136745fe3587dbfc186898dda5d496
xie <2951542805@qq.com>
2019-12-12 11:46:45 +0800

    项目创建

---
+++ src/main/java/com/chu/xieproject/xieproject/XieprojectApplication.java
@@ -0,0 +1,13 @@
+package com.chu.xieproject.xieproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XieprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XieprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chu/xieproject/xieproject/XieprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chu.xieproject.xieproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XieprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
